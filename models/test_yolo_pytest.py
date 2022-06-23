3b57893d811132249f43702adab2bce82cd3ef51
Seeeeeyo <selimgilon@gmail.com>
2022-06-23 14:43:32 -0500

    Code cleaning

--- models/test_yolo_pytest.py
+++ models/test_yolo_pytest.py
@@ -4,69 +4,148 @@ import torch
 
 ########################################
 shapes_base = {
-    0: [4, 32, 320, 320], 1: [4, 64, 160, 160], 2: [4, 64, 160, 160], 3: [4, 128, 80, 80],
-    4: [4, 128, 80, 80], 5: [4, 256, 40, 40], 6: [4, 256, 40, 40], 7: [4, 512, 20, 20],
-    8: [4, 512, 20, 20], 9: [4, 512, 20, 20], 10: [4, 256, 20, 20], 11: [4, 256, 40, 40],
-    12: [4, 512, 40, 40], 13: [4, 256, 40, 40], 14: [4, 128, 40, 40], 15: [4, 128, 80, 80],
-    16: [4, 256, 80, 80], 17: [4, 128, 80, 80], 18: [4, 128, 40, 40], 19: [4, 256, 40, 40],
-    20: [4, 256, 40, 40], 21: [4, 256, 20, 20], 22: [4, 512, 20, 20], 23: [4, 512, 20, 20]
+    0: [4, 32, 320, 320],
+    1: [4, 64, 160, 160],
+    2: [4, 64, 160, 160],
+    3: [4, 128, 80, 80],
+    4: [4, 128, 80, 80],
+    5: [4, 256, 40, 40],
+    6: [4, 256, 40, 40],
+    7: [4, 512, 20, 20],
+    8: [4, 512, 20, 20],
+    9: [4, 512, 20, 20],
+    10: [4, 256, 20, 20],
+    11: [4, 256, 40, 40],
+    12: [4, 512, 40, 40],
+    13: [4, 256, 40, 40],
+    14: [4, 128, 40, 40],
+    15: [4, 128, 80, 80],
+    16: [4, 256, 80, 80],
+    17: [4, 128, 80, 80],
+    18: [4, 128, 40, 40],
+    19: [4, 256, 40, 40],
+    20: [4, 256, 40, 40],
+    21: [4, 256, 20, 20],
+    22: [4, 512, 20, 20],
+    23: [4, 512, 20, 20]
     # No layer added
 }
 cfg_base = "yolov5s_base.yaml"
 ########################################
 shapes_avgpool_all_fm = {
-    0: [4, 32, 320, 320], 1: [4, 64, 160, 160], 2: [4, 64, 160, 160], 3: [4, 128, 80, 80],
-    4: [4, 128, 80, 80], 5: [4, 256, 40, 40], 6: [4, 256, 40, 40], 7: [4, 512, 20, 20],
-    8: [4, 512, 20, 20], 9: [4, 512, 20, 20], 10: [4, 256, 20, 20], 11: [4, 256, 40, 40],
-    12: [4, 512, 40, 40], 13: [4, 256, 40, 40], 14: [4, 128, 40, 40], 15: [4, 128, 80, 80],
-    16: [4, 256, 80, 80], 17: [4, 128, 80, 80], 18: [4, 128, 40, 40], 19: [4, 256, 40, 40],
-    20: [4, 256, 40, 40], 21: [4, 256, 20, 20], 22: [4, 512, 20, 20], 23: [4, 512, 20, 20],
+    0: [4, 32, 320, 320],
+    1: [4, 64, 160, 160],
+    2: [4, 64, 160, 160],
+    3: [4, 128, 80, 80],
+    4: [4, 128, 80, 80],
+    5: [4, 256, 40, 40],
+    6: [4, 256, 40, 40],
+    7: [4, 512, 20, 20],
+    8: [4, 512, 20, 20],
+    9: [4, 512, 20, 20],
+    10: [4, 256, 20, 20],
+    11: [4, 256, 40, 40],
+    12: [4, 512, 40, 40],
+    13: [4, 256, 40, 40],
+    14: [4, 128, 40, 40],
+    15: [4, 128, 80, 80],
+    16: [4, 256, 80, 80],
+    17: [4, 128, 80, 80],
+    18: [4, 128, 40, 40],
+    19: [4, 256, 40, 40],
+    20: [4, 256, 40, 40],
+    21: [4, 256, 20, 20],
+    22: [4, 512, 20, 20],
+    23: [4, 512, 20, 20],
     # added layers
-    24: [4, 256, 1, 1], 25: [4, 128, 1, 1], 26: [4, 128, 1, 1], 27: [4, 512, 1, 1],
+    24: [4, 256, 1, 1],
+    25: [4, 128, 1, 1],
+    26: [4, 128, 1, 1],
+    27: [4, 512, 1, 1],
     # detection head
-    28: [4, 3]
+    28: [4, 3],
 }
 cfg_avgpool_all_fm = "yolov5s_avgpool_all_fm.yaml"
 ##########################################
 shapes_complex = {
-    0: [4, 32, 320, 320], 1: [4, 64, 160, 160], 2: [4, 64, 160, 160], 3: [4, 128, 80, 80],
-    4: [4, 128, 80, 80], 5: [4, 256, 40, 40], 6: [4, 256, 40, 40], 7: [4, 512, 20, 20],
-    8: [4, 512, 20, 20], 9: [4, 512, 20, 20], 10: [4, 256, 20, 20], 11: [4, 256, 40, 40],
-    12: [4, 512, 40, 40], 13: [4, 256, 40, 40], 14: [4, 128, 40, 40], 15: [4, 128, 80, 80],
-    16: [4, 256, 80, 80], 17: [4, 128, 80, 80], 18: [4, 128, 40, 40], 19: [4, 256, 40, 40],
-    20: [4, 256, 40, 40], 21: [4, 256, 20, 20], 22: [4, 512, 20, 20], 23: [4, 512, 20, 20],
+    0: [4, 32, 320, 320],
+    1: [4, 64, 160, 160],
+    2: [4, 64, 160, 160],
+    3: [4, 128, 80, 80],
+    4: [4, 128, 80, 80],
+    5: [4, 256, 40, 40],
+    6: [4, 256, 40, 40],
+    7: [4, 512, 20, 20],
+    8: [4, 512, 20, 20],
+    9: [4, 512, 20, 20],
+    10: [4, 256, 20, 20],
+    11: [4, 256, 40, 40],
+    12: [4, 512, 40, 40],
+    13: [4, 256, 40, 40],
+    14: [4, 128, 40, 40],
+    15: [4, 128, 80, 80],
+    16: [4, 256, 80, 80],
+    17: [4, 128, 80, 80],
+    18: [4, 128, 40, 40],
+    19: [4, 256, 40, 40],
+    20: [4, 256, 40, 40],
+    21: [4, 256, 20, 20],
+    22: [4, 512, 20, 20],
+    23: [4, 512, 20, 20],
     # added layers
-    24: [4, 128, 20, 20], 25: [4, 128, 40, 40], 26: [4, 128, 40, 40], 27: [4, 384, 40, 40],
+    24: [4, 128, 20, 20],
+    25: [4, 128, 40, 40],
+    26: [4, 128, 40, 40],
+    27: [4, 384, 40, 40],
     # detection head
-    28: [4, 3]
+    28: [4, 3],
 }
 cfg_complex = "yolov5s_complex.yaml"
 ########################################
 shapes_avgpool_fm_large = {
-    0: [4, 32, 320, 320], 1: [4, 64, 160, 160], 2: [4, 64, 160, 160], 3: [4, 128, 80, 80],
-    4: [4, 128, 80, 80], 5: [4, 256, 40, 40], 6: [4, 256, 40, 40], 7: [4, 512, 20, 20],
-    8: [4, 512, 20, 20], 9: [4, 512, 20, 20], 10: [4, 256, 20, 20], 11: [4, 256, 40, 40],
-    12: [4, 512, 40, 40], 13: [4, 256, 40, 40], 14: [4, 128, 40, 40], 15: [4, 128, 80, 80],
-    16: [4, 256, 80, 80], 17: [4, 128, 80, 80], 18: [4, 128, 40, 40], 19: [4, 256, 40, 40],
-    20: [4, 256, 40, 40], 21: [4, 256, 20, 20], 22: [4, 512, 20, 20], 23: [4, 512, 20, 20],
+    0: [4, 32, 320, 320],
+    1: [4, 64, 160, 160],
+    2: [4, 64, 160, 160],
+    3: [4, 128, 80, 80],
+    4: [4, 128, 80, 80],
+    5: [4, 256, 40, 40],
+    6: [4, 256, 40, 40],
+    7: [4, 512, 20, 20],
+    8: [4, 512, 20, 20],
+    9: [4, 512, 20, 20],
+    10: [4, 256, 20, 20],
+    11: [4, 256, 40, 40],
+    12: [4, 512, 40, 40],
+    13: [4, 256, 40, 40],
+    14: [4, 128, 40, 40],
+    15: [4, 128, 80, 80],
+    16: [4, 256, 80, 80],
+    17: [4, 128, 80, 80],
+    18: [4, 128, 40, 40],
+    19: [4, 256, 40, 40],
+    20: [4, 256, 40, 40],
+    21: [4, 256, 20, 20],
+    22: [4, 512, 20, 20],
+    23: [4, 512, 20, 20],
     # added layers
     24: [4, 256, 1, 1],
     # detection head
-    25: [4, 3]
+    25: [4, 3],
 }
 cfg_avgpool_fm_large = "yolov5s_avgpool_fm_large.yaml"
 
 
 ########################################
 
+
 def validate_yolo5s(shapes=shapes_base, cfg="yolov5s_base.yaml"):
     cfg = cfg
-    device = 'cuda'
+    device = "cuda"
     batch = 4
     shapes_detect = {
         0: [4, 3, 80, 80, 85],
         1: [4, 3, 40, 40, 85],
-        2: [4, 3, 20, 20, 85]
+        2: [4, 3, 20, 20, 85],
     }
 
     x = torch.rand(batch, 3, 640, 640).to(device)
@@ -76,7 +155,11 @@ def validate_yolo5s(shapes=shapes_base, cfg="yolov5s_base.yaml"):
     # print("Added layers:")
     for m in model.model:
         if m.f != -1:  # if not from previous layer
-            x = y[m.f] if isinstance(m.f, int) else [x if j == -1 else y[j] for j in m.f]  # from earlier layers
+            x = (
+                y[m.f]
+                if isinstance(m.f, int)
+                else [x if j == -1 else y[j] for j in m.f]
+            )  # from earlier layers
         x = m(x)  # run
         if m._get_name() != "Detect":
             assert list(x.shape) == shapes[m.i]
@@ -97,7 +180,7 @@ def validate_yolo5s(shapes=shapes_base, cfg="yolov5s_base.yaml"):
 def test_yolo5s_base():
     # this is simply the basic Yolov5 small model
     model = validate_yolo5s(shapes=shapes_base, cfg=cfg_base)
-    assert model.model[24]._get_name() == 'Detect'
+    assert model.model[24]._get_name() == "Detect"
 
 
 def test_yolo5s_avgpool_all_fm():
@@ -105,11 +188,11 @@ def test_yolo5s_avgpool_all_fm():
     # 2) Then concatenated these 1x1x256 -- 1x1x128 -- 1x1x128 together to result into 1x1x512.
     # 3) Then squeezed this 1x1x512 and finally applied a Linear layer for classification
     model = validate_yolo5s(shapes=shapes_avgpool_all_fm, cfg=cfg_avgpool_all_fm)
-    assert model.model[24]._get_name() == 'AdaptiveAvgPool2d'
-    assert model.model[25]._get_name() == 'AdaptiveAvgPool2d'
-    assert model.model[26]._get_name() == 'AdaptiveAvgPool2d'
-    assert model.model[27]._get_name() == 'Concat'
-    assert model.model[28]._get_name() == 'LinearSqueeze'
+    assert model.model[24]._get_name() == "AdaptiveAvgPool2d"
+    assert model.model[25]._get_name() == "AdaptiveAvgPool2d"
+    assert model.model[26]._get_name() == "AdaptiveAvgPool2d"
+    assert model.model[27]._get_name() == "Concat"
+    assert model.model[28]._get_name() == "LinearSqueeze"
 
 
 def test_yolo5s_complex():
@@ -124,14 +207,14 @@ def test_yolo5s_complex():
     # 5.3) flatten the result
     model = validate_yolo5s(shapes=shapes_complex, cfg=cfg_complex)
     # fm-large
-    assert model.model[24]._get_name() == 'Conv'
-    assert model.model[25]._get_name() == 'Upsample'
+    assert model.model[24]._get_name() == "Conv"
+    assert model.model[25]._get_name() == "Upsample"
     # fm-small
-    assert model.model[26]._get_name() == 'AvgPool2d'
+    assert model.model[26]._get_name() == "AvgPool2d"
     # concat all fm
-    assert model.model[27]._get_name() == 'Concat'
+    assert model.model[27]._get_name() == "Concat"
 
-    assert model.model[28]._get_name() == 'Classify'
+    assert model.model[28]._get_name() == "Classify"
 
 
 def test_yolov5s_avgpool_fm_large():
@@ -139,6 +222,6 @@ def test_yolov5s_avgpool_fm_large():
     # Squeezed the result and applied a Linear layer
     model = validate_yolo5s(shapes=shapes_avgpool_fm_large, cfg=cfg_avgpool_fm_large)
     # fm-large
-    assert model.model[24]._get_name() == 'AdaptiveAvgPool2d'
+    assert model.model[24]._get_name() == "AdaptiveAvgPool2d"
 
-    assert model.model[25]._get_name() == 'LinearSqueeze'
+    assert model.model[25]._get_name() == "LinearSqueeze"
--- utils/loss.py
+++ utils/loss.py
@@ -10,7 +10,9 @@ from utils.metrics import bbox_iou
 from utils.torch_utils import de_parallel
 
 
-def smooth_BCE(eps=0.1):  # https://github.com/ultralytics/yolov3/issues/238#issuecomment-598028441
+def smooth_BCE(
+    eps=0.1,
+):  # https://github.com/ultralytics/yolov3/issues/238#issuecomment-598028441
     # return positive, negative label smoothing BCE targets
     return 1.0 - 0.5 * eps, 0.5 * eps
 
@@ -19,7 +21,9 @@ class BCEBlurWithLogitsLoss(nn.Module):
     # BCEwithLogitLoss() with reduced missing label effects.
     def __init__(self, alpha=0.05):
         super().__init__()
-        self.loss_fcn = nn.BCEWithLogitsLoss(reduction='none')  # must be nn.BCEWithLogitsLoss()
+        self.loss_fcn = nn.BCEWithLogitsLoss(
+            reduction="none"
+        )  # must be nn.BCEWithLogitsLoss()
         self.alpha = alpha
 
     def forward(self, pred, true):
@@ -40,7 +44,7 @@ class FocalLoss(nn.Module):
         self.gamma = gamma
         self.alpha = alpha
         self.reduction = loss_fcn.reduction
-        self.loss_fcn.reduction = 'none'  # required to apply FL to each element
+        self.loss_fcn.reduction = "none"  # required to apply FL to each element
 
     def forward(self, pred, true):
         loss = self.loss_fcn(pred, true)
@@ -54,9 +58,9 @@ class FocalLoss(nn.Module):
         modulating_factor = (1.0 - p_t) ** self.gamma
         loss *= alpha_factor * modulating_factor
 
-        if self.reduction == 'mean':
+        if self.reduction == "mean":
             return loss.mean()
-        elif self.reduction == 'sum':
+        elif self.reduction == "sum":
             return loss.sum()
         else:  # 'none'
             return loss
@@ -70,7 +74,7 @@ class QFocalLoss(nn.Module):
         self.gamma = gamma
         self.alpha = alpha
         self.reduction = loss_fcn.reduction
-        self.loss_fcn.reduction = 'none'  # required to apply FL to each element
+        self.loss_fcn.reduction = "none"  # required to apply FL to each element
 
     def forward(self, pred, true):
         loss = self.loss_fcn(pred, true)
@@ -80,9 +84,9 @@ class QFocalLoss(nn.Module):
         modulating_factor = torch.abs(true - pred_prob) ** self.gamma
         loss *= alpha_factor * modulating_factor
 
-        if self.reduction == 'mean':
+        if self.reduction == "mean":
             return loss.mean()
-        elif self.reduction == 'sum':
+        elif self.reduction == "sum":
             return loss.sum()
         else:  # 'none'
             return loss
@@ -97,35 +101,51 @@ class ComputeLoss:
         h = model.hyp  # hyperparameters
 
         # Define criteria
-        BCEcls = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['cls_pw']], device=device))
-        BCEobj = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([h['obj_pw']], device=device))
+        BCEcls = nn.BCEWithLogitsLoss(
+            pos_weight=torch.tensor([h["cls_pw"]], device=device)
+        )
+        BCEobj = nn.BCEWithLogitsLoss(
+            pos_weight=torch.tensor([h["obj_pw"]], device=device)
+        )
 
         CEloss = nn.CrossEntropyLoss()
 
         # Class label smoothing https://arxiv.org/pdf/1902.04103.pdf eqn 3
-        self.cp, self.cn = smooth_BCE(eps=h.get('label_smoothing', 0.0))  # positive, negative BCE targets
+        self.cp, self.cn = smooth_BCE(
+            eps=h.get("label_smoothing", 0.0)
+        )  # positive, negative BCE targets
         # TODO ? Label smoothing for new cls
 
         # Focal loss
-        g = h['fl_gamma']  # focal loss gamma
+        g = h["fl_gamma"]  # focal loss gamma
         if g > 0:
             BCEcls, BCEobj = FocalLoss(BCEcls, g), FocalLoss(BCEobj, g)
             # TODO ? Add focal loss
             # BCEcls_2 = FocalLoss(BCEcls_2, g)
 
         m = de_parallel(model).model[-1]  # Detect() module
-        self.balance = {3: [4.0, 1.0, 0.4]}.get(m.nl, [4.0, 1.0, 0.25, 0.06, 0.02])  # P3-P7
+        self.balance = {3: [4.0, 1.0, 0.4]}.get(
+            m.nl, [4.0, 1.0, 0.25, 0.06, 0.02]
+        )  # P3-P7
         self.ssi = list(m.stride).index(16) if autobalance else 0  # stride 16 index
-        self.BCEcls, self.BCEobj, self.gr, self.hyp, self.autobalance = BCEcls, BCEobj, 1.0, h, autobalance
+        self.BCEcls, self.BCEobj, self.gr, self.hyp, self.autobalance = (
+            BCEcls,
+            BCEobj,
+            1.0,
+            h,
+            autobalance,
+        )
         self.CEloss = CEloss
         self.na = m.na  # number of anchors
         self.nc = m.nc  # number of classes
         self.nl = m.nl  # number of layers
-        #print('num of layers', self.nl)
+        # print('num of layers', self.nl)
         self.anchors = m.anchors
         self.device = device
 
-    def __call__(self, preds, targets_det, targets_cls):  # predictions, detection targets, classification targets
+    def __call__(
+        self, preds, targets_det, targets_cls
+    ):  # predictions, detection targets, classification targets
         # TODO verify this and adapt the model(input) return format if needed
         (det_pred, cls_pred) = preds
         # detection losses
@@ -134,8 +154,10 @@ class ComputeLoss:
         lobj = torch.zeros(1, device=self.device)  # object loss
 
         # Useless for now
-        #lcls_mtl = torch.zeros(1, device= self.device) # classif loss
-        tcls, tbox, indices, anchors = self.build_targets(det_pred, targets_det)  # targets
+        # lcls_mtl = torch.zeros(1, device= self.device) # classif loss
+        tcls, tbox, indices, anchors = self.build_targets(
+            det_pred, targets_det
+        )  # targets
 
         # Scene classification loss (for 1 classification now, will extend it later)
         # lcls_mtl = torch.zeros(1, device=self.device)  # class loss (scene classification)
@@ -145,20 +167,25 @@ class ComputeLoss:
         for i, pi in enumerate(det_pred):  # layer index, layer predictions
             # print('pi.shape', pi.shape)
             b, a, gj, gi = indices[i]  # image, anchor, gridy, gridx
-            tobj = torch.zeros(pi.shape[:4], dtype=pi.dtype, device=self.device)  # target obj
-
+            tobj = torch.zeros(
+                pi.shape[:4], dtype=pi.dtype, device=self.device
+            )  # target obj
 
             n = b.shape[0]  # number of targets
             # print("number of targets", n)
             if n:
                 # pxy, pwh, _, pcls = pi[b, a, gj, gi].tensor_split((2, 4, 5), dim=1)  # faster, requires torch 1.8.0
-                pxy, pwh, _, pcls = pi[b, a, gj, gi].split((2, 2, 1, self.nc), 1)  # target-subset of predictions
+                pxy, pwh, _, pcls = pi[b, a, gj, gi].split(
+                    (2, 2, 1, self.nc), 1
+                )  # target-subset of predictions
 
                 # Regression
                 pxy = pxy.sigmoid() * 2 - 0.5
                 pwh = (pwh.sigmoid() * 2) ** 2 * anchors[i]
                 pbox = torch.cat((pxy, pwh), 1)  # predicted box
-                iou = bbox_iou(pbox, tbox[i], CIoU=True).squeeze()  # iou(prediction, target)
+                iou = bbox_iou(
+                    pbox, tbox[i], CIoU=True
+                ).squeeze()  # iou(prediction, target)
                 lbox += (1.0 - iou).mean()  # iou loss
 
                 # Objectness
@@ -183,38 +210,53 @@ class ComputeLoss:
             obji = self.BCEobj(pi[..., 4], tobj)
             lobj += obji * self.balance[i]  # obj loss
             if self.autobalance:
-                self.balance[i] = self.balance[i] * 0.9999 + 0.0001 / obji.detach().item()
+                self.balance[i] = (
+                    self.balance[i] * 0.9999 + 0.0001 / obji.detach().item()
+                )
         if self.autobalance:
             self.balance = [x / self.balance[self.ssi] for x in self.balance]
-        lbox *= self.hyp['box']
-        lobj *= self.hyp['obj']
-        lcls *= self.hyp['cls']
+        lbox *= self.hyp["box"]
+        lobj *= self.hyp["obj"]
+        lcls *= self.hyp["cls"]
         bs = tobj.shape[0]  # batch size
 
-        #print('lcls_mtl', lcls_mtl)
+        # print('lcls_mtl', lcls_mtl)
         # TODO check if the lcls_mtl has to be multiplied by "bs" as the other losses or not
         # TODO verify if the cat works (night not due to the different shapes)
-        return (lbox + lobj + lcls) * bs, lcls_mtl*bs, torch.cat((lbox, lobj, lcls)).detach(), lcls_mtl.detach()
+        return (
+            (lbox + lobj + lcls) * bs,
+            lcls_mtl * bs,
+            torch.cat((lbox, lobj, lcls)).detach(),
+            lcls_mtl.detach(),
+        )
 
     def build_targets(self, p, targets):
         # Build targets for compute_loss(), input targets(image,class,x,y,w,h)
         na, nt = self.na, targets.shape[0]  # number of anchors, targets
         tcls, tbox, indices, anch = [], [], [], []
         gain = torch.ones(7, device=self.device)  # normalized to gridspace gain
-        ai = torch.arange(na, device=self.device).float().view(na, 1).repeat(1, nt)  # same as .repeat_interleave(nt)
-        targets = torch.cat((targets.repeat(na, 1, 1), ai[..., None]), 2)  # append anchor indices
+        ai = (
+            torch.arange(na, device=self.device).float().view(na, 1).repeat(1, nt)
+        )  # same as .repeat_interleave(nt)
+        targets = torch.cat(
+            (targets.repeat(na, 1, 1), ai[..., None]), 2
+        )  # append anchor indices
 
         g = 0.5  # bias
-        off = torch.tensor(
-            [
-                [0, 0],
-                [1, 0],
-                [0, 1],
-                [-1, 0],
-                [0, -1],  # j,k,l,m
-                # [1, 1], [1, -1], [-1, 1], [-1, -1],  # jk,jm,lk,lm
-            ],
-            device=self.device).float() * g  # offsets
+        off = (
+            torch.tensor(
+                [
+                    [0, 0],
+                    [1, 0],
+                    [0, 1],
+                    [-1, 0],
+                    [0, -1],  # j,k,l,m
+                    # [1, 1], [1, -1], [-1, 1], [-1, -1],  # jk,jm,lk,lm
+                ],
+                device=self.device,
+            ).float()
+            * g
+        )  # offsets
 
         # print("nl", self.nl)
         for i in range(self.nl):
@@ -228,7 +270,7 @@ class ComputeLoss:
             if nt:
                 # Matches
                 r = t[..., 4:6] / anchors[:, None]  # wh ratio
-                j = torch.max(r, 1 / r).max(2)[0] < self.hyp['anchor_t']  # compare
+                j = torch.max(r, 1 / r).max(2)[0] < self.hyp["anchor_t"]  # compare
                 # j = wh_iou(anchors, t[:, 4:6]) > model.hyp['iou_t']  # iou(3,n)=wh_iou(anchors(3,2), gwh(n,2))
                 t = t[j]  # filter
 
@@ -251,7 +293,9 @@ class ComputeLoss:
             gi, gj = gij.T  # grid indices
 
             # Append
-            indices.append((b, a, gj.clamp_(0, shape[2] - 1), gi.clamp_(0, shape[3] - 1)))  # image, anchor, grid
+            indices.append(
+                (b, a, gj.clamp_(0, shape[2] - 1), gi.clamp_(0, shape[3] - 1))
+            )  # image, anchor, grid
             tbox.append(torch.cat((gxy - gij, gwh), 1))  # box
             anch.append(anchors[a])  # anchors
             tcls.append(c)  # class
